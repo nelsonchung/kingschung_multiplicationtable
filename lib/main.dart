90e6b3e335f6e24bacea32f69121402f75fd1e36
Nelson Chung <chihchun.chung@gmail.com>
2023-10-20 20:17:49 +0800

    改成前置鏡頭，Quality=100

--- lib/main.dart
+++ lib/main.dart
@@ -35,9 +35,11 @@ class _MyHomePageState extends State<MyHomePage> {
   final ImagePicker _picker = ImagePicker();
 
   _imgFromCamera() async {
+
     XFile? image = await _picker.pickImage(
       source: ImageSource.camera,
-      imageQuality: 50,
+      imageQuality: 100,
+      preferredCameraDevice: CameraDevice.front
     );
 
     setState(() {
